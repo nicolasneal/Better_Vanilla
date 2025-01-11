96912946b1187881b1bccfd49dcd9e95c01a66e9
Nicolas Neal <nicolasneal@gmail.com>
2025-01-10 22:14:51 -0600

    New commit

--- src/main/java/net/nicolas/bettervanilla/BetterVanilla.java
+++ src/main/java/net/nicolas/bettervanilla/BetterVanilla.java
@@ -2,6 +2,7 @@ package net.nicolas.bettervanilla;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.nicolas.bettervanilla.block.ModBlocks;
 import net.nicolas.bettervanilla.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class BetterVanilla implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
